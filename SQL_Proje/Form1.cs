074b081a8dbc26ce33b2d814451505bad6475119
FeelsBadMan0 <ahmetrafetseyhan@gmail.com>
2023-05-02 19:16:51 +0300

    Add project files.

---
+++ SQL_Proje/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SQL_Proje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-PNOIT9G\\SQLEXPRESS;Initial Catalog=Test;Integrated Security=True");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sorgu = richTextBox1.Text;
+
+
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter(sorgu, baglanti);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Sorgunuzu Kontrol Edin!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sorgu = richTextBox1.Text;
+            try
+            {
+
+                baglanti.Open();
+                SqlCommand komut = new SqlCommand(sorgu, baglanti);
+                komut.ExecuteNonQuery();
+                baglanti.Close();
+
+                SqlDataAdapter da = new SqlDataAdapter("Select * from TBLURUNLER", baglanti);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Lütfen Sorgunuzu Kontrol Edin!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
